d7cffe4313e8f258892acb5e2b6dcabf393ef30e
kuznetsovandrey76 <and.rey.q@yandex.ru>
2019-01-24 06:26:16 +0300

    ts l.1

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,19 @@
-function greeter(person: string) {
-    return "Hello, " + person;
+let isDone: boolean = true;
+let age: number = 28;
+let fullname: string = "Andrey Kuznetsov";
+
+let message: string = `Hello ${fullname}`;
+
+console.log(message);
+
+let year: string[]; // массив строковых значений
+year = ['Jan', 'Feb', 'Mar'];
+
+// Кортеж - упорядоченный набор фиксированной длины
+let x: [string, string, number] = ['Andrey', 'Kuznetsov', 28];
+
+function add(x: number, y: number) :number {
+    return x + y;
 }
 
-let user = "Andrey Kuznetsov!";
-
-document.body.innerHTML = greeter(user);
+// this - контекст функции. This - переменная, которая устанавливается при запуске функции.
